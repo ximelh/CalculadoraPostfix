9094ed906ba9c69f347d17183b1c07f4a54bf2e6
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-01-27 19:57:40 -0600

    Metodos del stack

--- StackGrupo10.java
+++ StackGrupo10.java
@@ -5,37 +5,114 @@ Clase stack grupo 10
 @file StackGrupo10.java
 
 ******************************************************************/
+import java.util.*;
 
-public class StackGrupo10 implements Stack {
+public class StackGrupo10<E> implements Stack<E> {
 
+    private Vector<E> stack;// = new Vector<E>();
+    private int top;
+
+    /**
+     * Constructor.
+     * Inicializa el stack
+     * Top es -1
+     */
+    public StackGrupo10(){
+        this.stack = new Vector<E>();
+        this.top = -1;
+    }
+
+    /**
+     * Indica el stack
+     * @return stack vector
+     */
+    public Vector<E> getStack() {
+        return this.stack;
+    }
+
+    /**
+     * Indica el top
+     * @return top int
+     */
+    public int getTop() {
+        return this.top;
+    }
+
+    /**
+     * Establece el stack
+     * @param stack
+     */
+    public void setStack(Vector<E> v) {
+        this.stack = v;
+    }
+
+    /**
+     *  Establece el top
+     * @param int
+     */
+    public void setTop(int i) {
+        this.top = i;
+    }
+
+        
+    /**
+     * Agrega un elemento al stack
+     */
     @Override
     public void add(E item) {
         // TODO Auto-generated method stub
-        
+        this.stack.addElement(item);
+        this.top++;
     }
 
+    /**
+     * Elimina un elemento del stack
+     * regresa el elemento eliminado
+     */
     @Override
     public E remove() {
         // TODO Auto-generated method stub
-        return null;
+        if (stack.isEmpty()){
+            return null;
+        } else {
+            E e = stack.lastElement();
+            this.stack.removeElementAt(top);
+            this.top--;
+            return e;
+        }
+
     }
 
+    /**
+     * Regresa el ultimo elemento del stack
+     */
     @Override
     public E peek() {
         // TODO Auto-generated method stub
-        return null;
+        if (stack.isEmpty()){
+            return null;
+        } else {
+            return stack.lastElement();
+        }
     }
 
+
+    /**
+     * Revisa si el stack esta vacio
+     */
     @Override
     public boolean empty() {
         // TODO Auto-generated method stub
-        return false;
+        return stack.isEmpty();
     }
 
+    /**
+     * Revisa cuantos elementos hay en el stack
+     */
     @Override
     public int size() {
         // TODO Auto-generated method stub
-        return 0;
+        return stack.size();
     }
 
 }
