19ca04d703d4db658b30df771bbb2f4fcaabf0f0
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-01-30 15:19:45 -0600

    Update CalculadoraGrupo10.java

--- CalculadoraGrupo10.java
+++ CalculadoraGrupo10.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.atomic.DoubleAccumulator;
+
 /******************************************************************
 Ultima modificacion: 27/01/2022
 
@@ -11,7 +13,34 @@ public class CalculadoraGrupo10 implements Calculator {
     @Override
     public double calculate(String operation) {
         // TODO Auto-generated method stub
-        return 0;
+        String[] caracteres = operation.split(" ");
+        StackGrupo10<Double> stack = new StackGrupo10<Double>();
+
+        for (String caracter:caracteres){
+            try { // si es numero
+                stack.add(Double.valueOf(Integer.parseInt(caracter)));
+
+            } catch (Exception e){  //si es caracter + - * /
+                if (caracter.equalsIgnoreCase("+")){ //sumar
+                    double num1 = stack.remove();
+                    double num2 = stack.remove();
+                    stack.add(num1+num2);
+                } else if (caracter.equalsIgnoreCase("-")){ // restar
+                    double num1 = stack.remove();
+                    double num2 = stack.remove();
+                    stack.add(num2-num1);
+                } else if (caracter.equalsIgnoreCase("/")){ //dividir
+                    double num1 = stack.remove();
+                    double num2 = stack.remove();
+                    stack.add(num2/num1);
+                } else if (caracter.equalsIgnoreCase("*")){ // multiplicar
+                    double num1 = stack.remove();
+                    double num2 = stack.remove();
+                    stack.add(num2*num1);
+                }
+            }
+        }
+        return stack.peek();
     }
     
 }
