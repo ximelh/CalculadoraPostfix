d87cd765c3778f4d476634cc9133515284aa3bcc
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-01-31 09:49:22 -0600

    agregar pruebas unitarias del stack

--- CalculadoraGrupo10Test.java
+++ CalculadoraGrupo10Test.java
@@ -21,5 +21,29 @@ class CalculadoraGrupo10Test {
 		assertEquals(30,calculadora.calculate("6 2 3 + *"),0.001,"Error en Calculate");
 		assertEquals(11,calculadora.calculate("10 3 5 + - 2 + 3 * 4 5 - +"),0.001,"Error en Calculate");
 	}
-
+	
+	/**
+	 * Evalua agregar y quitar elementos del stack
+	 */
+	@Test
+	void testAddRemove() {
+		StackGrupo10<Double> stack = new StackGrupo10<Double>();
+		stack.add(1.0);
+		stack.add(2.0);
+		assertEquals(2,stack.remove(),0.001,"Error al quitar elemento del stack");
+		assertEquals(1,stack.remove(),0.001,"Error al quitar elemento del stack");
+		
+	}
+	
+	/**
+	 * Evalua obtener elementos del stack y cuantos elementos hay
+	 */
+	@Test
+	void testPeekSize() {
+		StackGrupo10<Double> stack = new StackGrupo10<Double>();
+		stack.add(1.0);
+		stack.add(2.0);
+		assertEquals(2,stack.peek(),0.001,"Error al obtener objeto del stack");
+		assertEquals(2,stack.size(),0.001,"Error al obtener objeto del stack");
+	}
 }
