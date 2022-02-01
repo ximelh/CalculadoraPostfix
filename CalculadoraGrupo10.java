656fa0cf812521f54d9acdfc0cb7ce555a8fe07b
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-02-01 11:32:33 -0600

    organizar espacio en el código

--- CalculadoraGrupo10.java
+++ CalculadoraGrupo10.java
@@ -1,11 +1,7 @@
-
-
 /******************************************************************
 Ultima modificacion: 27/01/2022
-
 Clase calculadora grupo 10
 @file CalculadoraGrupo10.java
-
 ******************************************************************/
 
 public class CalculadoraGrupo10 implements Calculator{
@@ -22,34 +18,29 @@ public class CalculadoraGrupo10 implements Calculator{
                 stack.add(Double.valueOf(Integer.parseInt(caracter)));
 
             } catch (Exception e){  //si es caracter + - * /
-                if (caracter.equalsIgnoreCase("+")){ //sumar
-                	
+                if (caracter.equalsIgnoreCase("+")){ //sumar	
                 		double num1 = stack.remove();
                 		double num2 = stack.remove();
                 		stack.add(num1+num2);
                 		res = num1+num2;
                 
                 } else if (caracter.equalsIgnoreCase("-")){ // restar
-                	
                 		double num1 = stack.remove();
                 		double num2 = stack.remove();
                 		stack.add(num2-num1);
                 		res = num2-num1;
                 	
                 } else if (caracter.equalsIgnoreCase("/")){ //dividir
-                	
                 		double num1 = stack.remove();
                 		double num2 = stack.remove();
                 		stack.add(num2/num1);
                 		res = num2/num1;
                 	
                 } else if (caracter.equalsIgnoreCase("*")){ // multiplicar
-                	
                 		double num1 = stack.remove();
                 		double num2 = stack.remove();
                 		stack.add(num2*num1);
                 		res = num2*num1;
-                	
                 } else {
                 	throw e;
                 }
@@ -57,5 +48,4 @@ public class CalculadoraGrupo10 implements Calculator{
         }
         return res;
     }
-    
 }
--- CalculadoraGrupo10Test.java
+++ CalculadoraGrupo10Test.java
@@ -6,7 +6,6 @@
 
  ******************************************************************/
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class CalculadoraGrupo10Test {
